effdaacf4c7a24f49c3a401feb1d64ef722da3cc
Nathaniel Sabanski <sabanski.n@gmail.com>
2021-03-19 17:12:48 -0700

    Added comment.

--- new_key.sh
+++ new_key.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Generate new keys to RSA 4096 bit standard.
 # This can be upgraded to ed25519 when dropbear supports it.
 # Usage: ./new_key.sh domain.com
@@ -9,17 +8,18 @@
 set -e
 
 if [ "$1" ]; then
-	CHOICE=$1
+    # Non-Interactive mode.
+    CHOICE=$1
 else
-	# Interactive mode.
-	echo "This will generate new SSH keys. Enter the domain or service name. Example: yourwebsite.com"
-	read -r CHOICE
+    # Interactive mode.
+    echo "This will generate new SSH keys. Enter the domain or service name. Example: yourwebsite.com"
+    read -r CHOICE
 fi
 
 CHOICE=${CHOICE,,*( )} # Lowercase and trim.
 if [[ ("$CHOICE" == "" || ${#CHOICE} -le 5 ) ]]; then
-	echo "'${CHOICE}' is too short or invalid."
-	exit 1
+    echo "'${CHOICE}' is too short or invalid."
+    exit 1
 fi
 
 # Auto overwrite is "<<< y"
